ed6abff863c1d71460fd330bbb3c6deaf2083905
jpchung <jpchungjpc@gmail.com>
2018-06-13 21:38:15 -0600

    Added Card/CardSection reusable components, thumbnails and album images now showing

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,71 @@
 // import library to create component
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 // create compoent (functional) - only shows data
 //passed prop from parent component
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  //destructured props
+  const { title, artist, thumbnail_image, image } = album
+  const {
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    imageStyle
+  } = styles;
+
+  //will pass AlbumDetail to Card, CardSection components for styling
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={thumbnailStyle}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
+      </CardSection>
+    </Card>
   );
 };
 
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
 
 // make component available to other parts of app
 export default AlbumDetail;
